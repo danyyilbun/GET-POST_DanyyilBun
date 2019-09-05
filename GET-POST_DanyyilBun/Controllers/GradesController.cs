203de23f5c4b11be76ae0d2bceb1e5a35201346a
danyyilbun <48656385+danyyilbun@users.noreply.github.com>
2019-09-05 10:38:57 -0400

    Kinda Works Wntrie, But JS is commented out

--- GET-POST_DanyyilBun/Controllers/GradesController.cs
+++ GET-POST_DanyyilBun/Controllers/GradesController.cs
@@ -9,12 +9,40 @@ namespace GET_POST_DanyyilBun.Controllers
     public class GradesController : Controller
     {
         // GET: Grades
-        public ActionResult Index(FormCollection myForm)
+        [HttpGet]
+        public ActionResult Grades()
         {
-            foreach (var x in myForm)
-            {
+            ViewBag.Entries = 3;
+            return View();
+        }
 
-            }
+        [HttpPost]
+        public ActionResult Grades(FormCollection myForm)
+        {
+            //Double myNum;
+            //foreach (var x in myForm)
+            //{
+            //     if (Double.TryParse(x, out myNum))
+                
+               
+            //}
+            Double one,two,three;
+            Double.TryParse(myForm["grade0"], out one);
+            Double.TryParse(myForm["grade1"], out two);
+            Double.TryParse(myForm["grade2"], out three);
+            var total = (one + two + three)/3;
+            if (total >= 90)
+            { ViewBag.Letter = "A"; }
+            else if (total >= 80)
+            { ViewBag.Letter = "B"; }
+            else if (total >= 70)
+            { ViewBag.Letter = "C"; }
+            else if (total >= 60)
+            { ViewBag.Letter = "D"; }
+            else
+            { ViewBag.Letter = "F"; }
+            ViewBag.Total = total;
+            ViewBag.Entries = 3;
             return View();
         }
     }
